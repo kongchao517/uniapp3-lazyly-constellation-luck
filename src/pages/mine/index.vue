1af35b3a4c4bbd72c44ae67031f2d4123ecb86e1
kongchao <1871170192@qq.com>
2023-05-30 14:33:32 +0800

    特性: 🚀 h5头部导航删除及分栏使用

--- src/pages/mine/index.vue
+++ src/pages/mine/index.vue
@@ -4,10 +4,18 @@
  * @ created_at: 2023-05-29 13:52:43
  * @ modified_record:
  * @ modified_by: kongchao
- * @ modified_time: 2023-05-29 13:54:07
+ * @ modified_time: 2023-05-30 14:15:15
 -->
 <template>
-  <div>我的</div>
+  <div class="box">
+    <match-media :min-width="375" :max-width="800">
+      <view>当页面最小宽度 375px， 页面宽度最大 800px 时显示</view>
+    </match-media>
+
+    <match-media :min-height="400" :orientation="landscape">
+      <view>当页面高度不小于 400px 且屏幕方向为横向时展示这里</view>
+    </match-media>
+  </div>
   <TabBar />
 </template>
 
@@ -15,4 +23,8 @@
 import TabBar from '@components/TabBar';
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.box {
+  width: 1000rpx;
+}
+</style>
